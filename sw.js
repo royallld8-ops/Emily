50d1409c6027ad6798e9d9a84fd55e00d6fe7bda
royallld8-ops <royallld8@gmail.com>
2026-02-15 02:52:30 +0330

    Create sw.js
    const CACHE = "emily-v4";
    
    self.addEventListener("install", e => {
      e.waitUntil(
        caches.open(CACHE).then(c =>
          c.addAll([
            "./",
            "./index.html",
            "./style.css",
            "./app.js"
          ])
        )
      );
    });
    
    self.addEventListener("fetch", e => {
      e.respondWith(
        caches.match(e.request).then(r => r || fetch(e.request))
      );
    });

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "emily-v4";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
